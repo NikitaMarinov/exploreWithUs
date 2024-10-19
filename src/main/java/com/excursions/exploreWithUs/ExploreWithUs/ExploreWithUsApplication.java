09e1293d413da13182cf5916d06c417d2b8becb7
NikitaMarinov <nik.marinoff.1@gmail.com>
2024-10-19 17:58:09 +0300

    initial commit

---
+++ src/main/java/com/excursions/exploreWithUs/ExploreWithUs/ExploreWithUsApplication.java
@@ -0,0 +1,13 @@
+package com.excursions.exploreWithUs.ExploreWithUs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreWithUsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploreWithUsApplication.class, args);
+	}
+
+}
